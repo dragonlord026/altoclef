203b3435ddbbc5ef997945fedfd125af89763c5a
JGreen <72313560+jamesgreen31@users.noreply.github.com>
2022-04-29 22:50:05 -0400

    Updated Feature Request Issue Template
    Added a way for users to provide their discord username if they want to be more involved.

--- src/main/java/adris/altoclef/commands/TestCommand.java
+++ src/main/java/adris/altoclef/commands/TestCommand.java
@@ -6,16 +6,15 @@ import adris.altoclef.commandsystem.Arg;
 import adris.altoclef.commandsystem.ArgParser;
 import adris.altoclef.commandsystem.Command;
 import adris.altoclef.commandsystem.CommandException;
+import adris.altoclef.tasks.BonemealNearbyGrassTask;
+
 
 public class TestCommand extends Command {
-
-    public TestCommand() throws CommandException {
-        super("test", "Generic command for testing", new Arg(String.class, "extra", "", 0));
-    }
+    public TestCommand() {super("test", "Generic command for testing");}
 
     @Override
-    protected void call(AltoClef mod, ArgParser parser) throws CommandException {
-        Playground.TEMP_TEST_FUNCTION(mod, parser.get(String.class));
-        finish();
+    protected void call(AltoClef altoClef, ArgParser parser) throws CommandException {
+        altoClef.log("Whas Boppin!");
+        altoClef.runUserTask(new BonemealNearbyGrassTask(), this::finish);
     }
 }
